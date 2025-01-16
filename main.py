219d4068f78cc607f260ca6e3020d266c41100d3
Taveeporn Chuaybumrung <taweeporn16078@gmail.com>
2025-01-16 16:41:09 +0700

    Add button

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 
 Window.maximize()
 
 
-class MainWidget(Widget):
+class Screenmanager(ScreenManager):
+    pass
+
+
+class SettingScreen(Screen):
+    pass
+
+
+class MainScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
-        self._keyboard.bind(on_key_down=self._on_key_down)
-        self._keyboard.bind(on_key_up=self._on_key_up)
-        self.pressed_keys = set()
-
-    def _on_keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_key_down)
-        self._keyboard.unbind(on_key_up=self._on_key_up)
-        self._keyboard = None
-
-    def _on_key_down(self, keyboard, keycode, text, modifiers):
-        print("down", text)
-        self.pressed_keys.add(text)
-
-    def _on_key_up(self, keyboard, keycode):
-        text = keycode[1]
-        print("up", text)
-
-        if text in self.pressed_keys:
-            self.pressed_keys.remove(text)
 
     def close_app(self):
         App.get_running_app().stop()
@@ -36,7 +25,7 @@ class MainWidget(Widget):
 
 class MyApp(App):
     def build(self):
-        return MainWidget()
+        return Builder.load_file("my.kv")
 
 
 if __name__ == "__main__":
