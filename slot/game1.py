d60880d823d4fe933817f4a49ed0cb9d75b4cbb4
Taveeporn Chuaybumrung <taweeporn16078@gmail.com>
2025-01-18 14:25:14 +0700

    add character move

--- slot/game1.py
+++ slot/game1.py
@@ -3,20 +3,49 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
+from kivy.clock import Clock
 import os
-import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 Window.maximize()
 
 
 class Play1(Widget):
-    def write(self):
-        print("Play!")
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    def close_app(self):
-        App.get_running_app().stop()
-        os.remove("slot/slot1_copy.kv")
+        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_key_down)
+        self._keyboard.bind(on_key_up=self._on_key_up)
+        self.pressed_keys = set()
+        Clock.schedule_interval(self.update, 0)
+
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard.unbind(on_key_up=self._on_key_up)
+        self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        print("down", text)
+        self.pressed_keys.add(text)
+
+    def _on_key_up(self, keyboard, keycode):
+        text = keycode[1]
+        print("up", text)
+
+        if text in self.pressed_keys:
+            self.pressed_keys.remove(text)
+
+    def update(self, dt):
+        step = 1000 * dt
+
+        if "w" in self.pressed_keys:
+            self.ids.object.pos[1] -= step
+        if "s" in self.pressed_keys:
+            self.ids.object.pos[1] += step
+        if "a" in self.pressed_keys:
+            self.ids.object.pos[0] += step
+        if "d" in self.pressed_keys:
+            self.ids.object.pos[0] -= step
 
 
 class Game1(App):
